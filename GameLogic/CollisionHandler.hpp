fb78a2f1a699c06a2975f8f0effa1b13dbb86090
Said Rafili <therafilisaid@gmail.com>
2024-12-22 11:35:30 +0100

    Make collision handler methods private

--- GameLogic/CollisionHandler.hpp
+++ GameLogic/CollisionHandler.hpp
@@ -10,13 +10,13 @@ class CollisionHandler
 {
     private:
         void HandleBallAndBrickCollision(Ball &ball, std::vector<Brick> &bricks);
-    
+        void HandleBallAndWallCollision(Ball &ball);
+        void HandleBallAndPaddleCollision(Ball &ball, Paddle &paddle);
+
     public:
         CollisionHandler();
         ~CollisionHandler();
         void HandleCollisions(Paddle &paddle, Ball &ball, std::vector<Brick> &bricks);
-        void HandleBallAndWallCollision(Ball &ball);
-        void HandleBallAndPaddleCollision(Ball &ball, Paddle &paddle);
 };
 
 #endif
